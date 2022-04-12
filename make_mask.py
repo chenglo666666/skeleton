4814c624f58edba32aa125f5505e73f416504d1b
chenglo666666 <56257384+chenglo666666@users.noreply.github.com>
2022-04-12 21:55:20 +0800

    Update make_mask.py

--- make_mask.py
+++ make_mask.py
@@ -3,6 +3,7 @@ import numpy as np
 import cv2
 import json
 import os
+import sys
 
 hand1 = np.zeros((21, 2))
 hand2 = np.zeros((21, 2))
@@ -16,10 +17,10 @@ def draw_line(hand, num1, num2, c):
         y_label = [hand[i, 1], hand[i + 1, 1]]
         plt.plot(x_label, y_label, color=c)
 
-# 顯示圖片
+# 畫骨架、標點
 
 
-def show_img(img, length):
+def draw_skeleton(img, length):
     image_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     plt.scatter(hand1[:, 0], hand1[:, 1], c='r')
     draw_line(hand1, 0, 4, 'lightgreen')
@@ -59,21 +60,40 @@ def show_img(img, length):
         for i in range(21):
             plt.annotate(str(i), (hand2[i, 0], hand2[i, 1]), color='white')
     plt.imshow(image_rgb)
-    # fig1 = plt.gcf()
     plt.show()
-    # fig1.savefig('save_00000.png')
+
+# 畫mask、dilate、儲存圖片
+
+
+def make_mask(img, length, store_location, store_name):
     if length == 21:
         hand_1 = np.array(hand1, dtype=np.int32)
-        #filled = np.zeros_like(img)
         filled = cv2.fillPoly(img, pts=[hand_1], color=(255, 255, 255))
-        cv2.imwrite('hand_mask.jpg', filled)
+        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        plt.axis('off')
+        plt.imshow(img_rgb)
+        fig1 = plt.gcf()
+        # plt.show()
+        fig1.savefig(store_location + '\\' + store_name, bbox_inches='tight',
+                     transparent=True, pad_inches=0)
     elif length == 42:
         hand_1 = np.array(hand1, dtype=np.int32)
         hand_2 = np.array(hand2, dtype=np.int32)
-        #filled = np.zeros_like(img)
         filled = cv2.fillPoly(img, pts=[hand_1], color=(
             255, 255, 255)) + cv2.fillPoly(img, pts=[hand_2], color=(255, 255, 255))
-        cv2.imwrite('hand_mask.jpg', filled)
+        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        plt.axis('off')
+        plt.imshow(img_rgb)
+        fig1 = plt.gcf()
+        # plt.show()
+        fig1.savefig(store_location + '\\' + store_name, bbox_inches='tight',
+                     transparent=True, pad_inches=0)
+    img1_loc = store_location + '\\' + store_name
+    img1 = cv2.imread(img1_loc)
+    kernal = np.ones((2, 2), np.uint8)
+    dilate = cv2.dilate(img1, kernal, iterations=1)
+    cv2.imwrite(store_location + '\\' + store_name, dilate)
+
 # 處理點
 
 
@@ -95,31 +115,50 @@ def json_data_segmentation(data):
 '''
 frames = os.listdir('.\\frames')
 for frame in frames:
-    for img in os.listdir(frame):
-        image = cv2.imread(f'{frame}/{img}')
+    for img in os.listdir('.\\frames\\' + frame):
+        image = cv2.imread(f'.\\frames\\{frame}\\{img}')
+
+labels = os.listdir('.\\label_landmark')
+for label in labels:
+    for js in os.listdir('.\\label_landmark\\' + label):
+        with open()
 '''
 
+# frames : frames中的資料夾位置list
+# labels : label_landmark中的資料夾位置list
+# frame : 圖片位置list
+# label : json檔list
 
-img_loc = '.\\frames\\00000\\00000.png'
-json_loc = ".\\label_landmark\\00000\\00000.json"
+
+frames = os.listdir('.\\frames')
+labels = os.listdir('.\\label_landmark')
+for i in range(0, len(frames)):
+    frame = os.listdir('.\\frames\\' + frames[i])
+    label = os.listdir('.\\label_landmark\\' + labels[i])
+    save_loc = '.\\output_mask\\' + frames[i]
+    for j in range(0, len(frame)):
+        save_name = frame[j]
+        origin_img = cv2.imread('.\\frames\\' + frames[i] + '\\' + frame[j])
+        with open('.\\label_landmark\\' + labels[i] + '\\' + label[j]) as f:
+            json_data = json.load(f)
+        json_data_segmentation(json_data)
+        make_mask(origin_img, len(json_data), save_loc, save_name)
+
+'''
+img_loc = '.\\frames\\00000\\00023.png'
+json_loc = ".\\label_landmark\\00000\\00023.json"
 # 讀取圖片 laod image
 origin_img = cv2.imread(img_loc)
 
 
 with open(json_loc) as f:
     json_data = json.load(f)
-print(json_data)
+# print(json_data)
 json_data_segmentation(json_data)
-print(hand1)
-print(hand2)
+# print(hand1)
+# print(hand2)
 
-show_img(origin_img, len(json_data))
+# draw_skeleton(origin_img, len(json_data))
 
-'''
-img1_loc = 'hand_mask.jpg'
-img1 = cv2.imread(img1_loc)
-kernal = np.ones((2, 2), np.uint8)
-dilate = cv2.dilate(img1, kernal, iterations=1)
-cv2.imshow('result', dilate)
-cv2.waitKey(0)
+# make_mask(origin_img, len(json_data))
 '''
