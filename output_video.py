6b29fee8f7e1c31b3a4e4924b7f652d1fbb60a65
chenglo666666 <56257384+chenglo666666@users.noreply.github.com>
2022-04-13 21:45:44 +0800

    Update output_video.py

--- output_video.py
+++ output_video.py
@@ -1,17 +1,20 @@
 import cv2
 import numpy as np
 import glob
+import os
 
-img_array = []
-for filename in glob.glob('.\\output_mask\\*.jpg'):
-    img = cv2.imread(filename)
-    height, width, layers = img.shape
-    size = (width, height)
-    img_array.append(img)
+masks = os.listdir('.\\used\\output')
+for i in range(0, len(masks)):
+    img_array = []
+    for filename in glob.glob('.\\used\\output\\' + masks[i] + '\\*.png'):
+        img = cv2.imread(filename)
+        height, width, layers = img.shape
+        size = (width, height)
+        img_array.append(img)
 
+    out = cv2.VideoWriter(
+        '.\\videos\\' + 'mask_' + masks[i] + '.avi', cv2.VideoWriter_fourcc(*'XVID'), 30, size)
 
-out = cv2.VideoWriter('mask.avi', cv2.VideoWriter_fourcc(*'XVID'), 30, size)
-
-for i in range(len(img_array)):
-    out.write(img_array[i])
-out.release()
+    for i in range(len(img_array)):
+        out.write(img_array[i])
+    out.release()
