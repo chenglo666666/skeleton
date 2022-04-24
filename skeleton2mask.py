34b03a2cb5ae3373e5aaae0a878683ff04a4b80a
chenglo666666 <56257384+chenglo666666@users.noreply.github.com>
2022-04-25 03:33:53 +0800

    Update skeleton2mask.py

--- skeleton2mask.py
+++ skeleton2mask.py
@@ -5,6 +5,41 @@ import json
 import numpy as np
 import matplotlib.pyplot as plt
 
+# convex hull algorithm implementation
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+def convex_hull(points, length):
+    min = 0
+    for i in range(1, length):
+        if points[i].x < points[min].x:
+            min = i
+        elif points[i].x == points[min].x:
+            if points[i].y > points[min].y:
+                min = i
+    most_left_index = min
+    hull = []
+    p = most_left_index
+    q = 0
+    while True:
+        hull.append(p)
+        q = (p + 1) % length
+        for i in range(length):
+            val = (points[i].y - points[p].y) * (points[q].x - points[i].x) - \
+                   (points[i].x - points[p].x) * (points[q].y - points[i].y)
+            if val < 0:
+                q = i
+        p = q
+        if p == most_left_index:
+            break
+    return hull
+
+
 def json_data_segmentation(data):
     length = len(data)
     if length == 21:
@@ -19,136 +54,192 @@ def json_data_segmentation(data):
             hand2[i-21][0] = data[i]['x']
             hand2[i-21][1] = data[i]['y']
 
-def get_framesAmount(path):
-    return len(os.listdir(path))
 
-d_t = ['test']
-
-for dt in d_t:
-    landmarkRoot = f'../mediapipe/data/MultiColor_skeleton_totalFrames/WLASL_100/{dt}/label_landmark'
-    root_multiColorSkeleton = f'../mediapipe/data/Bbox_cropBySkeleton_totalFrames/WLASL_100/{dt}/frames'
-    outputMask = f'../mediapipe/data/Hand_Mask_totalFrames/WLASL_100/{dt}'
-    os.makedirs(outputMask, exist_ok=True)
+landmarkRoot = '.\\label_landmark'
+root_multiColorSkeleton = '.\\frames'
+outputMask = '.\\output'
+os.makedirs(outputMask, exist_ok=True)
 
 
-    hand1 = np.zeros((21, 2))
-    hand2 = np.zeros((21, 2))
+hand1 = np.zeros((21, 2))
+hand2 = np.zeros((21, 2))
 
 
+for video in sorted(os.listdir(root_multiColorSkeleton)):
+    v = root_multiColorSkeleton + '\\' + video
+    assert os.path.exists(v), 'video path is incorrect!'
+    framesAmount = len(os.listdir(v))
+    # paths
+    '''
+    maskPath = f'{outputMask}/mask'
+    os.makedirs(maskPath, exist_ok=True)
+    '''
+    cropPath = f'{outputMask}/cropped'
+    os.makedirs(cropPath, exist_ok=True)
+    '''
+    visualizePath = f'{outputMask}/visualize'
+    os.makedirs(visualizePath, exist_ok=True)
+    video_visPath = f'{outputMask}/vis_detectedPerformance'
+    os.makedirs(video_visPath, exist_ok=True)
+    '''
+    convex_hull_path = f'{outputMask}/ConvexHull'
+    os.makedirs(convex_hull_path, exist_ok=True)
+    # visualize video
+    '''
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    video_detectPerformance = cv2.VideoWriter(
+        f'{video_visPath}/detected_{video}.mp4', fourcc, 30, (224, 224))
+    '''
+    frame_idx = 0
+    for frame in sorted(os.listdir(v)):
+        image = cv2.imread(f'{v}/{frame}')
+        cropped = image.copy()
+        #mask = np.zeros(image.shape[:2], dtype=np.uint8)
 
-    for video in sorted(os.listdir(root_multiColorSkeleton)):
-        v = f'{root_multiColorSkeleton}/{video}'
-        assert os.path.exists(v), 'video path is incorrect!'
-        framesAmount = len(os.listdir(v))
-        # paths 
-        maskPath = f'{outputMask}/mask'
-        os.makedirs(maskPath, exist_ok=True)
-        cropPath = f'{outputMask}/cropped'
-        os.makedirs(cropPath, exist_ok=True)
-        visualizePath = f'{outputMask}/visualize'
-        os.makedirs(visualizePath, exist_ok=True)
-        video_visPath = f'{outputMask}/vis_detectedPerformance'
-        os.makedirs(video_visPath, exist_ok=True)
-        # visualize video
-        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-        video_detectPerformance = cv2.VideoWriter(f'{video_visPath}/detected_{video}.mp4', fourcc, 30, (224, 224))
-        frame_idx = 0
-        for frame in sorted(os.listdir(v)):
-            image = cv2.imread(f'{v}/{frame}')
-            cropped = image.copy()
-            mask = np.zeros(image.shape[:2], dtype=np.uint8)
-            
-            frame = frame.split('.')[0]
-            jsonFile = f'{landmarkRoot}/{video}/{frame}.json'
-            # assert os.path.exists(jsonFile), 'landmark file does not exist!!'s
-            if os.path.exists(jsonFile):
-                f = open(jsonFile, 'r')
-                landmarks = json.load(f)
-                # save points as numpy array
-                json_data_segmentation(landmarks)
-                
-                if len(landmarks) == 21:
-                    hand_1 = np.array(hand1, dtype=np.int32)
-                    crop = np.zeros(image.shape)
-                    mask = cv2.fillPoly(mask, pts=[hand_1], color=(8, 8, 8))
-                    drawed = cv2.fillPoly(image, pts=[hand_1], color=(0, 255, 0))
-                    
-                    x = [a[0] for a in hand_1]
-                    y = [a[1] for a in hand_1]
+        frame = frame.split('.')[0]
+        jsonFile = f'{landmarkRoot}/{video}/{frame}.json'
+        # assert os.path.exists(jsonFile), 'landmark file does not exist!!'s
+        if os.path.exists(jsonFile):
+            f = open(jsonFile, 'r')
+            landmarks = json.load(f)
+            # save points as numpy array
+            json_data_segmentation(landmarks)
+
+            if len(landmarks) == 21:
+                hand_1 = np.array(hand1, dtype=np.int32)
+                crop = np.zeros(image.shape)
+                '''
+                mask = cv2.fillPoly(mask, pts=[hand_1], color=(8, 8, 8))
+                drawed = cv2.fillPoly(image, pts=[hand_1], color=(0, 255, 0))
+                '''
+
+                x = [a[0] for a in hand_1]
+                y = [a[1] for a in hand_1]
+                x0, y0, x1, y1 = min(x), min(y), max(x), max(y)
+                crop[y0:y1, x0:x1, 0] = cropped[y0:y1, x0:x1, 0]
+                crop[y0:y1, x0:x1, 1] = cropped[y0:y1, x0:x1, 1]
+                crop[y0:y1, x0:x1, 2] = cropped[y0:y1, x0:x1, 2]
+
+                # do convex hull
+                points = []
+                for i in range(0, 21):
+                    points.append(Point(hand1[i][0], hand1[i][1]))
+                hull = convex_hull(points, 21)
+                outline1 = np.zeros((len(hull), 2))
+                for i in range(len(hull)):
+                    outline1[i][0] = hand1[hull[i]][0]
+                    outline1[i][1] = hand1[hull[i]][1]
+                '''
+                for i in hand_1:
+                    # mask number set to 8
+                    mask[i[1], i[0]] = 8
+                # save mask
+                os.makedirs(f'{maskPath}/{video}', exist_ok=True)
+                cv2.imwrite(f'{maskPath}/{video}/{frame}.png', mask)
+                '''
+                # save cropped hands
+                os.makedirs(f'{cropPath}/{video}', exist_ok=True)
+                cv2.imwrite(f'{cropPath}/{video}/crop_{frame}.png', crop)
+                '''
+                # save crop
+                cv2.putText(drawed, 'frame: {}'.format(frame_idx),
+                            (0, 50), cv2.FONT_HERSHEY_TRIPLEX, 0.5, (255, 0, 0))
+                img_rgb = cv2.cvtColor(drawed, cv2.COLOR_BGR2RGB)
+                plt.axis('off')
+                plt.imshow(img_rgb)
+                fig1 = plt.gcf()
+                os.makedirs(f'{visualizePath}/{video}', exist_ok=True)
+                fig1.savefig(f'{visualizePath}/{video}/{frame}.png',
+                             bbox_inches='tight', transparent=True, pad_inches=0)
+                video_detectPerformance.write(drawed)
+                '''
+            else:
+                # Case: 2 hands
+                hand_1 = np.array(hand1, dtype=np.int32)
+                hand_2 = np.array(hand2, dtype=np.int32)
+                crop = np.zeros(image.shape)
+                '''
+                mask = cv2.fillPoly(mask, pts=[hand_1], color=(8, 8, 8))
+                mask = cv2.fillPoly(mask, pts=[hand_2], color=(8, 8, 8))
+
+                drawed = cv2.fillPoly(image, pts=[hand_1], color=(0, 255, 0))
+                drawed = cv2.fillPoly(image, pts=[hand_2], color=(0, 255, 0))
+                '''
+                # drawed = cv2.fillPoly(image, pts=[hand_1], color=(255, 255, 255)) + cv2.fillPoly(image, pts=[hand_2], color=(255, 255, 255))
+                # crop 2 hands
+
+                for hand in [hand_1, hand_2]:
+                    x = [a[0] for a in hand]
+                    y = [a[1] for a in hand]
+                    '''
+                    for i in hand:
+                        # mask number set to 8
+                        mask[i[1], i[0]] = 8
+                    '''
                     x0, y0, x1, y1 = min(x), min(y), max(x), max(y)
                     crop[y0:y1, x0:x1, 0] = cropped[y0:y1, x0:x1, 0]
                     crop[y0:y1, x0:x1, 1] = cropped[y0:y1, x0:x1, 1]
                     crop[y0:y1, x0:x1, 2] = cropped[y0:y1, x0:x1, 2]
-                    for i in hand_1:
-                        # mask number set to 8
-                        mask[i[1], i[0]] = 8
-                    # save mask
-                    os.makedirs(f'{maskPath}/{video}', exist_ok=True)
-                    cv2.imwrite(f'{maskPath}/{video}/{frame}.png', mask)
-                    # save cropped hands
-                    os.makedirs(f'{cropPath}/{video}', exist_ok=True)
-                    cv2.imwrite(f'{cropPath}/{video}/crop_{frame}.png', crop)
-                    # save crop
-                    cv2.putText(drawed, 'frame: {}'.format(frame_idx), (0,50), cv2.FONT_HERSHEY_TRIPLEX, 0.5, (255, 0, 0))
-                    img_rgb = cv2.cvtColor(drawed, cv2.COLOR_BGR2RGB)
-                    plt.axis('off')
-                    plt.imshow(img_rgb)
-                    fig1 = plt.gcf()
-                    os.makedirs(f'{visualizePath}/{video}', exist_ok=True)
-                    fig1.savefig(f'{visualizePath}/{video}/{frame}.png', bbox_inches='tight', transparent=True, pad_inches=0)
-                    video_detectPerformance.write(drawed)
-                else:
-                    # Case: 2 hands
-                    hand_1 = np.array(hand1, dtype=np.int32)
-                    hand_2 = np.array(hand2, dtype=np.int32)
-                    crop = np.zeros(image.shape)
-                    mask = cv2.fillPoly(mask, pts=[hand_1], color=(8, 8, 8))
-                    mask = cv2.fillPoly(mask, pts=[hand_2], color=(8, 8, 8))
-                    
-                    drawed = cv2.fillPoly(image, pts=[hand_1], color=(0, 255, 0))
-                    drawed = cv2.fillPoly(image, pts=[hand_2], color=(0, 255, 0))
-                    
-                    # drawed = cv2.fillPoly(image, pts=[hand_1], color=(255, 255, 255)) + cv2.fillPoly(image, pts=[hand_2], color=(255, 255, 255))
-                    # crop 2 hands         
-                    for hand in [hand_1, hand_2]:
-                        x = [a[0] for a in hand]
-                        y = [a[1] for a in hand]
-                        for i in hand:
-                            # mask number set to 8
-                            mask[i[1], i[0]] = 8
-                        x0, y0, x1, y1 = min(x), min(y), max(x), max(y)
-                        crop[y0:y1, x0:x1, 0] = cropped[y0:y1, x0:x1, 0]
-                        crop[y0:y1, x0:x1, 1] = cropped[y0:y1, x0:x1, 1]
-                        crop[y0:y1, x0:x1, 2] = cropped[y0:y1, x0:x1, 2]
-                    # save mask
-                    os.makedirs(f'{maskPath}/{video}', exist_ok=True)
-                    cv2.imwrite(f'{maskPath}/{video}/{frame}.png', mask)
-                    # save crop hands
-                    os.makedirs(f'{cropPath}/{video}', exist_ok=True)
-                    cv2.imwrite(f'{cropPath}/{video}/crop_{frame}.png', crop)
-                    cv2.putText(drawed, 'frame: {}'.format(frame_idx), (0,50), cv2.FONT_HERSHEY_TRIPLEX, 0.5, (255, 0, 0))
-                    img_rgb = cv2.cvtColor(drawed, cv2.COLOR_BGR2RGB)
-                    plt.axis('off')
-                    plt.imshow(img_rgb)
-                    fig1 = plt.gcf()
-                    os.makedirs(f'{visualizePath}/{video}', exist_ok=True)
-                    fig1.savefig(f'{visualizePath}/{video}/{frame}.png', bbox_inches='tight', transparent=True, pad_inches=0)
-                    video_detectPerformance.write(drawed)
-                    
-                kernel = np.ones((2,2), np.uint8)
-                dilate = cv2.dilate(image, kernel, iterations=1)
-            else:
+
+                # do convex hull
+                # hand1
+                points = []
+                for i in range(0, 21):
+                    points.append(Point(hand1[i][0], hand1[i][1]))
+                hull = convex_hull(points, 21)
+                outline1 = np.zeros((len(hull), 2))
+                for i in range(len(hull)):
+                    outline1[i][0] = hand1[hull[i]][0]
+                    outline1[i][1] = hand1[hull[i]][1]
+                # hand2
+                points = []
+                for i in range(0, 21):
+                    points.append(Point(hand2[i][0], hand2[i][1]))
+                hull = convex_hull(points, 21)
+                outline2 = np.zeros((len(hull), 2))
+                for i in range(len(hull)):
+                    outline2[i][0] = hand2[hull[i]][0]
+                    outline2[i][1] = hand2[hull[i]][1]
+                '''
                 # save mask
                 os.makedirs(f'{maskPath}/{video}', exist_ok=True)
                 cv2.imwrite(f'{maskPath}/{video}/{frame}.png', mask)
-                # save black mask as crop hands
+                '''
+                # save crop hands
                 os.makedirs(f'{cropPath}/{video}', exist_ok=True)
-                cv2.imwrite(f'{cropPath}/{video}/crop_{frame}.png', mask)
-                cv2.putText(image, 'notDetected_frame: {}'.format(frame_idx), (0,50), cv2.FONT_HERSHEY_TRIPLEX, 0.5, (0, 0, 255))
-                # save video
-                video_detectPerformance.write(image)
-            frame_idx += 1
-        # check mask amount is epual to frames amount
-        if len(os.listdir(f'{maskPath}/{video}')) != framesAmount:
-            assert 'mask amount is incorrect!'
-        print(f'video: {video} is done.')
+                cv2.imwrite(f'{cropPath}/{video}/crop_{frame}.png', crop)
+                '''
+                cv2.putText(drawed, 'frame: {}'.format(frame_idx),
+                            (0, 50), cv2.FONT_HERSHEY_TRIPLEX, 0.5, (255, 0, 0))
+                img_rgb = cv2.cvtColor(drawed, cv2.COLOR_BGR2RGB)
+                plt.axis('off')
+                plt.imshow(img_rgb)
+                fig1 = plt.gcf()
+                os.makedirs(f'{visualizePath}/{video}', exist_ok=True)
+                fig1.savefig(f'{visualizePath}/{video}/{frame}.png',
+                             bbox_inches='tight', transparent=True, pad_inches=0)
+                video_detectPerformance.write(drawed)
+                '''
+        '''
+            kernel = np.ones((2, 2), np.uint8)
+            dilate = cv2.dilate(image, kernel, iterations=1)
+
+        else:
+            # save mask
+            os.makedirs(f'{maskPath}/{video}', exist_ok=True)
+            cv2.imwrite(f'{maskPath}/{video}/{frame}.png', mask)
+            # save black mask as crop hands
+            os.makedirs(f'{cropPath}/{video}', exist_ok=True)
+            cv2.imwrite(f'{cropPath}/{video}/crop_{frame}.png', mask)
+            cv2.putText(image, 'notDetected_frame: {}'.format(
+                frame_idx), (0, 50), cv2.FONT_HERSHEY_TRIPLEX, 0.5, (0, 0, 255))
+            # save video
+            video_detectPerformance.write(image)
+        '''
+        frame_idx += 1
+    '''
+    # check mask amount is epual to frames amount
+    if len(os.listdir(f'{maskPath}/{video}')) != framesAmount:
+        assert 'mask amount is incorrect!'
+    '''
+    print(f'video: {video} is done.')
