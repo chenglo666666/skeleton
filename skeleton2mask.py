02137f0062ffe3afb7f97b82b28bd9d98307cda6
chenglo666666 <56257384+chenglo666666@users.noreply.github.com>
2022-04-29 18:12:28 +0800

    Update skeleton2mask.py

--- skeleton2mask.py
+++ skeleton2mask.py
@@ -4,6 +4,83 @@ import cv2
 import json
 import numpy as np
 import matplotlib.pyplot as plt
+import random
+
+# draw lines for skeleton
+
+
+def draw_line(hand, num1, num2, c):
+    for i in range(num1, num2):
+        x_label = [hand[i, 0], hand[i + 1, 0]]
+        y_label = [hand[i, 1], hand[i + 1, 1]]
+        plt.plot(x_label, y_label, color=c)
+
+# draw skeleton
+
+
+def draw_skeleton(img, length, hand_1, hand_2):
+    plt.clf()
+    image_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    plt.scatter(hand_1[:, 0], hand_1[:, 1], c='r')
+    draw_line(hand_1, 0, 4, 'lightgreen')
+    draw_line(hand_1, 5, 8, 'lightgreen')
+    draw_line(hand_1, 9, 12, 'lightgreen')
+    draw_line(hand_1, 13, 16, 'lightgreen')
+    draw_line(hand_1, 17, 20, 'lightgreen')
+    plt.plot([hand_1[0, 0], hand_1[5, 0]], [
+             hand_1[0, 1], hand_1[5, 1]], color='lightgreen')
+    plt.plot([hand_1[0, 0], hand_1[17, 0]], [
+             hand_1[0, 1], hand_1[17, 1]], color='lightgreen')
+    plt.plot([hand_1[5, 0], hand_1[9, 0]], [
+             hand_1[5, 1], hand_1[9, 1]], color='lightgreen')
+    plt.plot([hand_1[9, 0], hand_1[13, 0]], [
+             hand_1[9, 1], hand_1[13, 1]], color='lightgreen')
+    plt.plot([hand_1[13, 0], hand_1[17, 0]], [
+             hand_1[13, 1], hand_1[17, 1]], color='lightgreen')
+    '''
+    for i in range(21):
+        plt.annotate(str(i), (hand1[i, 0], hand1[i, 1]), color='white')
+    '''
+    if length == 42:
+        plt.scatter(hand_2[:, 0], hand_2[:, 1], c='b')
+        draw_line(hand_2, 0, 4, 'yellow')
+        draw_line(hand_2, 5, 8, 'yellow')
+        draw_line(hand_2, 9, 12, 'yellow')
+        draw_line(hand_2, 13, 16, 'yellow')
+        draw_line(hand_2, 17, 20, 'yellow')
+        plt.plot([hand_2[0, 0], hand2[5, 0]], [
+                 hand_2[0, 1], hand_2[5, 1]], color='yellow')
+        plt.plot([hand_2[0, 0], hand_2[17, 0]], [
+                 hand_2[0, 1], hand_2[17, 1]], color='yellow')
+        plt.plot([hand_2[5, 0], hand_2[9, 0]], [
+                 hand_2[5, 1], hand_2[9, 1]], color='yellow')
+        plt.plot([hand_2[9, 0], hand_2[13, 0]], [
+                 hand_2[9, 1], hand_2[13, 1]], color='yellow')
+        plt.plot([hand_2[13, 0], hand_2[17, 0]], [
+                 hand_2[13, 1], hand_2[17, 1]], color='yellow')
+        '''
+        for i in range(21):
+            plt.annotate(str(i), (hand2[i, 0], hand2[i, 1]), color='white')
+        '''
+    plt.axis('off')
+    plt.imshow(image_rgb)
+    figure = plt.gcf()
+    return figure
+
+
+# random noises
+
+
+def random_noises(hand):
+    for i in range(0, len(hand)):
+        x_or_y = random.randint(0, 1)
+        shift_distance = random.randint(-1, 1)
+        if x_or_y == 0:
+            hand[i][0] += shift_distance
+        else:
+            hand[i][1] += shift_distance
+    return hand
+
 
 # convex hull algorithm implementation
 
@@ -86,6 +163,14 @@ for video in sorted(os.listdir(root_multiColorSkeleton)):
     os.makedirs(convex_hull_path, exist_ok=True)
     convex_hull_mask_path = f'{outputMask}/ConvexHullMask'
     os.makedirs(convex_hull_mask_path, exist_ok=True)
+    noise_visPath = f'{outputMask}/noises_detectedPerformance'
+    os.makedirs(noise_visPath, exist_ok=True)
+    noise_visualize_path = f'{outputMask}/noise_visualize'
+    os.makedirs(noise_visualize_path, exist_ok=True)
+    # visualize noises video
+    vid = cv2.VideoWriter_fourcc(*'mp4v')
+    video_noise = cv2.VideoWriter(
+        f'{noise_visPath}/noise_{video}.mp4', vid, 30, (244, 244))
     # visualize video
     '''
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
@@ -95,6 +180,7 @@ for video in sorted(os.listdir(root_multiColorSkeleton)):
     frame_idx = 0
     for frame in sorted(os.listdir(v)):
         image = cv2.imread(f'{v}/{frame}')
+        noise_img = image.copy()
         cropped = image.copy()
         convex_hull_cropped = image.copy()
         # mask = np.zeros(image.shape[:2], dtype=np.uint8)
@@ -153,6 +239,14 @@ for video in sorted(os.listdir(root_multiColorSkeleton)):
                 cv2.imwrite(
                     f'{convex_hull_mask_path}/{video}/{frame}.png', convex_hull_crop)
 
+                # save noise images, and then save those images as video
+                os.makedirs(f'{noise_visualize_path}/{video}', exist_ok=True)
+                noise_1 = random_noises(hand1)
+                noise_figure = draw_skeleton(noise_img, 21, noise_1, hand2)
+                noise_figure.savefig(f'{noise_visualize_path}/{video}/{frame}.png',
+                                     bbox_inches='tight', transparent=True, pad_inches=0)
+                video_noise.write(cv2.imread(
+                    f'{noise_visualize_path}/{video}/{frame}.png'))
                 '''
                 for i in hand_1:
                     # mask number set to 8
@@ -246,6 +340,16 @@ for video in sorted(os.listdir(root_multiColorSkeleton)):
                 cv2.imwrite(
                     f'{convex_hull_mask_path}/{video}/{frame}.png', convex_hull_crop)
 
+                # save noise images, and then save those images as video
+                os.makedirs(f'{noise_visualize_path}/{video}', exist_ok=True)
+                noise_1 = random_noises(hand1)
+                noise_2 = random_noises(hand2)
+                noise_figure = draw_skeleton(noise_img, 42, noise_1, noise_2)
+                noise_figure.savefig(f'{noise_visualize_path}/{video}/{frame}.png',
+                                     bbox_inches='tight', transparent=True, pad_inches=0)
+                video_noise.write(cv2.imread(
+                    f'{noise_visualize_path}/{video}/{frame}.png'))
+
                 '''
                 # save mask
                 os.makedirs(f'{maskPath}/{video}', exist_ok=True)
@@ -288,5 +392,4 @@ for video in sorted(os.listdir(root_multiColorSkeleton)):
     if len(os.listdir(f'{maskPath}/{video}')) != framesAmount:
         assert 'mask amount is incorrect!'
     '''
-    print(f'video: {video} is done.')
     print(f'video: {video} is done.')
